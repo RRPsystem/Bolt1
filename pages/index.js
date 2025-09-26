f1bef71c3f2b05785755a189067403e760a8d04a
RRPsystem <alex@rondreis-planner.nl>
2025-09-26 13:54:28 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "system-ui, sans-serif", padding: "32px" }}>
+      <h1>Bolt + Next.js werkt 🎉</h1>
+      <p>Dit is je minimale startproject. Pas gerust aan.</p>
+    </main>
+  );
+}
